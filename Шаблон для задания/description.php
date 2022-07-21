c3e0026801b0808acd9ca84a6a681cc505f1678a
Irakli <94283406+Irakli132@users.noreply.github.com>
2022-07-21 17:47:28 +0400

    FIRST TASK

---
+++ Шаблон для задания/description.php
@@ -0,0 +1,8 @@
+<?
+$arTemplate = array (
+  'NAME' => 'Task1',
+  'DESCRIPTION' => 'Шаблон для теста. Только и всего',
+  'SORT' => '',
+  'TYPE' => '',
+);
+?>
---
+++ Шаблон для задания/header.php
@@ -0,0 +1,38 @@
+<? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die(); ?>
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title><? $APPLICATION->ShowTitle(); ?></title>
+
+  <? $APPLICATION->ShowHead();  ?>
+
+  <!-- Font Awesome Icons -->
+  <link href="<?=SITE_TEMPLATE_PATH?>/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700&selection.subset=cyrillic" rel="stylesheet">
+  <link href='https://fonts.googleapis.com/css?family=Merriweather:300,300i,400,400i,700,700i&display=swap&subset=cyrillic' rel='stylesheet' type='text/css'>
+
+  <!-- Plugin CSS -->
+  <link href="<?=SITE_TEMPLATE_PATH?>/vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+  <!-- Theme CSS - Includes Bootstrap -->
+  <link href="<?=SITE_TEMPLATE_PATH?>/css/creative.min.css" rel="stylesheet">
+  
+</head>
+
+<body id="page-top">
+<div id="panel">
+    <? $APPLICATION->ShowPanel(); ?> 
+</div>
+
+
+
+  
